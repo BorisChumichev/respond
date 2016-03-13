0b599bc1d32517a464fbb51341eda9369c7e3c63
Boris Chumichev <boris362@yandex.ru>
2016-03-13 17:13:41 +0100

    Initial scaffolding

---
+++ client/components/Index/Index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class IndexComponent extends Component {
+  render() {
+    return (
+      <section>
+        <h2>react-webpack-boilerplate</h2>
+      </section>
+    );
+  }
+}
+
+IndexComponent.defaultProps = {};
+
+export default IndexComponent;
